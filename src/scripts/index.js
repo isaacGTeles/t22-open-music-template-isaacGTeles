fd2ace7dc5ee44b06a7614bd1c7224245e4af9d2
isaacGTeles <isaac.gabriel1999@gmail.com>
2025-03-27 17:12:29 -0300

    Tarefa 2 concluída

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,13 @@
 // script global
+
+const slider = document.querySelector('.preco-slider');
+
+slider.addEventListener('input', function () {
+  const min = slider.min;
+  const max = slider.max;
+  const val = slider.value;
+  
+  const percentage = ((val - min) / (max - min)) * 100;
+
+  slider.style.background = `linear-gradient(to right, var(--brand-1) 0%, var(--brand-1) ${percentage}%, var(--gray-5) ${percentage}%, var(--gray-5) 100%)`;
+});
